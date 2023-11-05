fc251cf9f1db29f9f6af6b1b652629feea51121d
Nguyễn Quang Vinh <139441823+Nqvinh7603@users.noreply.github.com>
2023-11-05 15:35:16 +0700

    init project

---
+++ src/main/java/com/employeemanagementsystem/EmployeeManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.employeemanagementsystem;
+
+/**
+ *
+ * @author Admin
+ */
+public class EmployeeManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
